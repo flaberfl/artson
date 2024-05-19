e99b2c87ed7daf9fe5b8b04ccb3d026eb4d38ca6
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2024-05-19 14:18:45 +0300

    home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,100 +3,100 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// ymaps.ready(init);
-// function init() {
-//   var map = new ymaps.Map("map", {
-//     center: [55.720860, 37.560354],
-//     zoom: 15
-//   });
+ymaps.ready(init);
+function init() {
+  var map = new ymaps.Map("map", {
+    center: [55.720860, 37.560354],
+    zoom: 15
+  });
 
-//   // var map2 = new ymaps.Map("map2", {
-//   //   center: [55.720860, 37.560354],
-//   //   zoom: 15
-//   // });
+  // var map2 = new ymaps.Map("map2", {
+  //   center: [55.720860, 37.560354],
+  //   zoom: 15
+  // });
 
-//   // var myPlacemark2 = new ymaps.Placemark(
+  // var myPlacemark2 = new ymaps.Placemark(
 
-//   //   [55.727540, 37.567785],
-//   //   {},
-//   //   {
-//   //     iconLayout: 'default#image',
-//   //     iconImageHref: './img/map/01-r.png',
-//   //     // iconImageSize: [59, 78],
-//   //     iconImageSize: [112, 122],
-//   //     iconImageOffset: [-30, -78]
-//   //   });
-
-
-//   map.controls.remove('geolocationControl');
-//   map.controls.remove('searchControl'); // удаляем поиск
-//   map.controls.remove('trafficControl'); // удаляем контроль трафика
-//   map.controls.remove('typeSelector'); // удаляем тип
-
-//   // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-//   map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-//   map.controls.remove('rulerControl'); // удаляем контрол правил
-//   map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  //   [55.727540, 37.567785],
+  //   {},
+  //   {
+  //     iconLayout: 'default#image',
+  //     iconImageHref: './img/map/01-r.png',
+  //     // iconImageSize: [59, 78],
+  //     iconImageSize: [112, 122],
+  //     iconImageOffset: [-30, -78]
+  //   });
 
 
-//   // map2.controls.remove('geolocationControl');
-//   // map2.controls.remove('searchControl'); // удаляем поиск
-//   // map2.controls.remove('trafficControl'); // удаляем контроль трафика
-//   // map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
 
-//   // // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-//   // map2.controls.remove('zoomControl'); // удаляем контрол зуммирования
-//   // map2.controls.remove('rulerControl'); // удаляем контрол правил
-//   // map2.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-
-//   // map.geoObjects.add(myPlacemark);
-// }
+  // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 
 
+  // map2.controls.remove('geolocationControl');
+  // map2.controls.remove('searchControl'); // удаляем поиск
+  // map2.controls.remove('trafficControl'); // удаляем контроль трафика
+  // map.controls.remove('typeSelector'); // удаляем тип
+
+  // // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  // map2.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  // map2.controls.remove('rulerControl'); // удаляем контрол правил
+  // map2.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  // map.geoObjects.add(myPlacemark);
+}
 
 
-// (function () {
-//   var a = document.querySelector('.turnkey__media'), b = null, P = 0;
-//   window.addEventListener('scroll', Ascroll, false);
-//   document.body.addEventListener('scroll', Ascroll, false);
-//   function Ascroll() {
-//     if (b == null) {
-//       var Sa = getComputedStyle(a, ''), s = '';
-//       for (var i = 0; i < Sa.length; i++) {
-//         if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-//           s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
-//         }
-//       }
-//       b = document.createElement('div');
-//       b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-//       a.insertBefore(b, a.firstChild);
-//       var l = a.childNodes.length;
-//       for (var i = 1; i < l; i++) {
-//         b.appendChild(a.childNodes[1]);
-//       }
-//       a.style.height = b.getBoundingClientRect().height + 'px';
-//       a.style.padding = '0';
-//       a.style.border = '0';
-//     }
-//     var Ra = a.getBoundingClientRect(),
-//       R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.turnkey__stages').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
-//     if ((Ra.top - P) <= 0) {
-//       if ((Ra.top - P) <= R) {
-//         b.className = 'stop';
-//         b.style.top = - R + 'px';
-//       } else {
-//         b.className = 'sticky';
-//         b.style.top = P + 'px';
-//       }
-//     } else {
-//       b.className = '';
-//       b.style.top = '';
-//     }
-//     window.addEventListener('resize', function () {
-//       a.children[0].style.width = getComputedStyle(a, '').width
-//     }, false);
-//   }
-// })()
+
+
+(function () {
+  var a = document.querySelector('.turnkey__media'), b = null, P = 0;
+  window.addEventListener('scroll', Ascroll, false);
+  document.body.addEventListener('scroll', Ascroll, false);
+  function Ascroll() {
+    if (b == null) {
+      var Sa = getComputedStyle(a, ''), s = '';
+      for (var i = 0; i < Sa.length; i++) {
+        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+          s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+        }
+      }
+      b = document.createElement('div');
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';
+      a.style.padding = '0';
+      a.style.border = '0';
+    }
+    var Ra = a.getBoundingClientRect(),
+      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.turnkey__stages').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+    if ((Ra.top - P) <= 0) {
+      if ((Ra.top - P) <= R) {
+        b.className = 'stop';
+        b.style.top = - R + 'px';
+      } else {
+        b.className = 'sticky';
+        b.style.top = P + 'px';
+      }
+    } else {
+      b.className = '';
+      b.style.top = '';
+    }
+    window.addEventListener('resize', function () {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false);
+  }
+})()
 
 
 
